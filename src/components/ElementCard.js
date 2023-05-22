fcd05dd04d768bf32c057fe8a2997ac77c9730c6
Easton Meth <easton.meth@gmail.com>
2023-05-22 18:51:10 -0400

    Change image, additional styling

--- src/components/ElementCard.js
+++ src/components/ElementCard.js
@@ -2,7 +2,7 @@ export default function ElementCard(props) {
     return (
         <div className='card'>
             <h2 className='card-number'>{props.number}</h2>
-            <img className='card-image' src={props.bohr_model_image} alt='' />
+            <img className='card-image' src={props.image.url} alt=''/>
             <h2 className='card-symbol'>{props.symbol}</h2>
             <h3 className='card-name'>{props.name}</h3>
             <h4 className='card-mass'>{props.atomic_mass}</h4>
