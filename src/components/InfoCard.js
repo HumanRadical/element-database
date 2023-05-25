f27d7299f373270687a93c0c9798afda63862d1a
Easton Meth <easton.meth@gmail.com>
2023-05-25 19:42:50 -0400

    Styling, add u to atomic mass

--- src/components/InfoCard.js
+++ src/components/InfoCard.js
@@ -5,12 +5,12 @@ export default function InfoCard(props) {
             <img className='info-card-image' src={props.image.url} alt=''/>
             <h2 className='info-card-number'>Element number: {props.number}</h2>
             <div className="info-card-details">
-                <p>Symbol: {props.symbol}</p>
-                <p>Name: {props.name}</p>
-                <p>Appearance: {props.appearance ? props.appearance : "???"}</p>
-                <p>Phase at STP: {props.phase}</p>
-                <p>Atomic mass: {props.atomic_mass}</p>
-                <p>Summary: {props.summary}</p>
+                <p><strong>Symbol:</strong> {props.symbol}</p>
+                <p><strong>Name:</strong> {props.name}</p>
+                <p><strong>Appearance:</strong> {props.appearance ? props.appearance : "???"}</p>
+                <p><strong>Phase at STP:</strong> {props.phase}</p>
+                <p><strong>Atomic mass:</strong> {props.atomic_mass} u</p>
+                <p><strong>Summary:</strong> {props.summary}</p>
             </div>
         </div>
     )
