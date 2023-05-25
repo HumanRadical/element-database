334d8daaad17f64bd1fc289123df11aac226055f
Easton Meth <easton.meth@gmail.com>
2023-05-25 16:57:01 -0400

    Remove card mass

--- src/components/ElementCard.js
+++ src/components/ElementCard.js
@@ -5,7 +5,6 @@ export default function ElementCard(props) {
             <img className='card-image' src={props.image.url} alt=''/>
             <h2 className='card-symbol'>{props.symbol}</h2>
             <h3 className='card-name'>{props.name}</h3>
-            <h4 className='card-mass'>{props.atomic_mass}</h4>
         </div>
     )
 }
