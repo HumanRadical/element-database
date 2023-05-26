986a9d99dfb46f7c23c79808003a5c4e9cfc2ab8
Easton Meth <easton.meth@gmail.com>
2023-05-25 20:48:06 -0400

    Create vanilla folder and intialize html and js

---
+++ vanilla/index.js
@@ -0,0 +1,4 @@
+const bodyContainer = document.getElementById('body-container')
+const elementContainer = document.getElementById('element-container')
+const elementCardContainer = document.getElementById('element-card-container')
+const infoContainer = document.getElementById('info-container')
